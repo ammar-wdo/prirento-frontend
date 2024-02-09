d5ac7850ad25da553ca09242ff0e78cf4aafada3
PRIRENTO <prirento@wdodigitals-Mac-mini.local>
2024-02-09 11:29:21 +0400

    banner image

--- components/banner.tsx
+++ components/banner.tsx
@@ -5,12 +5,13 @@ type Props = {}
 
 const Banner = (props: Props) => {
   return (
-    <div className='bg-main px-12 sm:px-24 md:px-32 lg:px-52 h-[20vh] md:h-[40vh]  lg:h-[50vh] flex items-center  overflow-hidden lg:pb-20 flex-col sm:gap-12'>
-        <p className='capitalize text-center lg:text-5xl md:text-3xl text-xl text-white font-medium md:mt-12'>Find, book, and rental car<br/> in <span className='text-secondaryGold'>easy</span> steps.</p>
-     <div className='w-full max-w-[900px]  aspect-[16/7] relative mt-auto md:mt-0'>
-        <Image src={'/banner.png'} alt='banner' fill  />
+    <div className='bg-main md:pb-32 pb-56'>
+        <p className='capitalize text-white  text-center font-semibold pt-2 md:pt-12 text-2xl md:text-3xl lg:text-5xl'>find, book, and rental car<br/> in <span className='text-secondaryGold'>easy</span> steps.</p>
+        <div className='mt-2 sm:mt-4 mx-auto w-full aspect-[30.1/11] relative max-w-[900px]'>
+            <Image src={'/banner.png'} fill alt='banner image'/>
 
-     </div>
+        </div>
+      
       
     </div>
   )
