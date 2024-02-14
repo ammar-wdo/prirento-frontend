21cac62e647018fd8d7c416c0074a14f9fc5e21b
PRIRENTO <prirento@wdodigitals-Mac-mini.local>
2024-02-14 10:10:48 +0400

    slug

--- components/(search-component)/seatch-component-server-wrapper.tsx
+++ components/(search-component)/seatch-component-server-wrapper.tsx
@@ -7,7 +7,19 @@ import { LocationType } from '@/types'
 type Props = {}
 
 const SearchComponentServerWrapper = async(props: Props) => {
-  const locations = await fetcher<{locations:LocationType[]}>(GET_LOCATIONS).then(data=>data.locations)
+  // const locations = await fetcher<{locations:LocationType[]}>(GET_LOCATIONS).then(data=>data.locations)
+  const locations = [
+    {
+    id:'1',
+    name:"dubai",
+    slug:'dubai'
+  },
+    {
+    id:'2',
+    name:"abu dhabi",
+    slug:'abu-dhabi'
+  },
+]
 
   return (
    <SearchComponent locations={locations}/>
