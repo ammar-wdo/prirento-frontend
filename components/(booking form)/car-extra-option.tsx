d2ed27b855978ddd27457d88cfe8ac58cfec9aec
PRIRENTO <prirento@wdodigitals-Mac-mini.local>
2024-02-23 13:55:00 +0400

    extraoptions

--- components/(booking form)/car-extra-option.tsx
+++ components/(booking form)/car-extra-option.tsx
@@ -1,6 +1,6 @@
 import { cn } from "@/lib/utils";
 import { CarExtraOptions } from "@/types";
-import { Check } from "lucide-react";
+import { Check, Square } from "lucide-react";
 import Image from "next/image";
 import React from "react";
 
@@ -14,13 +14,17 @@ const CarExtraOption = ({ carExtraOption, active, handleClick }: Props) => {
   return (
     <div
       className={cn(
-        "p-4 rounded-md flex  items-center justify-between bg-muted cursor-pointer active:scale-[0.99] hover:opacity-80 transition",
-        active && "ring-2 ring-black"
+        "p-4 rounded-md flex  items-center justify-between hover:bg-muted border cursor-pointer active:scale-[0.99] hover:opacity-80 transition",
+        active && "   bg-muted"
       )}
       onClick={handleClick}
     >
-      <div className="flex items-center ">
-        {active && <Check className="mr-2" />}
+      <div className="flex items-center  ">
+        <span className="relative mr-2 w-5  h-5 rounded-sm flex items-center justify-center border border-black">
+         
+            {active && <Check className=" text-secondaryGreen w-5 h-5 absolute" />}
+        </span>
+       
         <p className="capitalize ">{carExtraOption.label}</p>
       </div>
       <div className="font-medium">{carExtraOption.price} AED</div>
