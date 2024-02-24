483f33c217cce2b7ab52b199f199238629324187
ammar <ammar@wdodigital.com>
2024-02-24 11:14:09 +0400

    refine

--- components/loading-component.tsx
+++ components/loading-component.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import animationData from '../public/lotti.json';
 import LottieAnimation from './lotti-wrapper'
 
@@ -5,9 +6,10 @@ type Props = {}
 
 const LoadingComponent = (props: Props) => {
   return (
-    <div className='flex items-center justify-center h-screen'>
-    <div className='w-[300px] aspect-square'>
-    <LottieAnimation animationData={animationData}/>
+    <div className='flex items-center justify-center h-screen '>
+    <div className='relative w-[300px] aspect-square animate-pulse'>
+    {/* <LottieAnimation animationData={animationData}/> */}
+    <Image alt='loader'  fill src={'/loader-logo.png'}/>
     </div>
      
   </div>
