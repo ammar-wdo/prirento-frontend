baaa58ea01488c82cc876811f9e693b8299d7509
ammar <ammar@wdodigital.com>
2024-02-26 07:14:00 +0400

    refine

--- components/loading-component.tsx
+++ components/loading-component.tsx
@@ -1,3 +1,4 @@
+import { Crown } from 'lucide-react';
 import Image from 'next/image';
 
 
@@ -6,12 +7,13 @@ type Props = {}
 
 const LoadingComponent = (props: Props) => {
   return (
-    <div className='flex items-center justify-center h-screen '>
+    <div className='flex items-center justify-center h-screen relative '>
+         <Crown className='w-44 h-44 text-secondaryGold opacity-45 absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[131%] ' />
     <div className='relative w-[300px] aspect-square animate-pulse'>
 
     <Image alt='loader'  fill src={'/loader-logo.png'}/>
     </div>
-     
+  
   </div>
   )
 }
