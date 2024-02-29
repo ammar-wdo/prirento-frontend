6d0cff860902bdd119ef484f307e7824d18b95df
ammar <ammar@wdodigital.com>
2024-02-29 12:04:33 +0400

    refine

--- components/(reviews)/review-swiper-slice.tsx
+++ components/(reviews)/review-swiper-slice.tsx
@@ -1,53 +1,64 @@
-'use client'
-import React, { useState } from 'react'
-import { SwiperSlide } from 'swiper/react'
-import { Card, CardContent, CardHeader } from '../ui/card'
-import ReactStars from 'react-stars'
-import { Review } from '@/types'
-import { formatDate } from '@/lib/utils'
-import Image from 'next/image'
-import Link from 'next/link'
-import { Button } from '../ui/button'
-import ReviewSlicer from './review-slicer'
+"use client";
+import React, { useState } from "react";
+import { SwiperSlide } from "swiper/react";
+import { Card, CardContent, CardHeader } from "../ui/card";
+import ReactStars from "react-stars";
+import { Review } from "@/types";
+import { formatDate } from "@/lib/utils";
+import Image from "next/image";
+import Link from "next/link";
+import { Button } from "../ui/button";
+import ReviewSlicer from "./review-slicer";
 
 type Props = {
-    i:number,
-    review: Review
-}
+  i: number;
+  review: Review;
+};
 
-const ReviewSwiperSlide = ({i,review}: Props) => {
- 
+const ReviewSwiperSlide = ({ i, review }: Props) => {
   return (
     <Card className="p-4  rounded-xl  border-none shadow-[1px_1px_5px] min-h-[200px]  shadow-gray-200">
-    <CardHeader className=" p-0  ">
+      <CardHeader className=" p-0  ">
         <div className="flex items-center justify-between">
-        <div className="flex items-center gap-3">
-        <h3 className="font-medium capitalize">{review.user}</h3>
-        <ReactStars
+          <div className="flex items-center gap-3">
+            <h3 className="font-medium capitalize">{review.user}</h3>
+            <ReactStars
               edit={false}
               size={14}
               color2="gold"
               color1="gray"
               count={5}
               value={review.rate}
-
-        />
-      </div>
-      <Link href={`/${review.companySlug}`}>
-      <div className='w-20 aspect-video relative'>
-        <Image src={review.companyLogo} alt='company logo' className='object-contain' fill/>
-      </div>
-      </Link>
-      
-   
+            />
+          </div>
+          <Link href={`/${review.companySlug}`}>
+            <div className="w-20 aspect-video relative">
+              <Image
+                src={review.companyLogo}
+                alt="company logo"
+                className="object-contain"
+                fill
+              />
+            </div>
+          </Link>
         </div>
-      <p>{formatDate(new Date(review.createdAt))}</p>
-    </CardHeader>
-    <CardContent className="text-xs p-0 mt-3 text-muted-foreground">
-     <ReviewSlicer reviewContent={review.reviewContent}/>
-    </CardContent>
-  </Card>
-  )
-}
+        <p>
+          {formatDate(new Date(review.createdAt), "en-GB", {
+            timeZone: "Asia/Dubai",
+            year: "numeric",
+            month: "2-digit",
+            day: "2-digit",
+            hour: "2-digit",
+            minute: "2-digit",
+            hour12: false,
+          })}
+        </p>
+      </CardHeader>
+      <CardContent className="text-xs p-0 mt-3 text-muted-foreground">
+        <ReviewSlicer reviewContent={review.reviewContent} />
+      </CardContent>
+    </Card>
+  );
+};
 
-export default ReviewSwiperSlide
+export default ReviewSwiperSlide;
