a510bcaae52e450895e79d8a330cb62082d24e92
ammar <ammar@wdodigital.com>
2024-03-05 09:22:27 +0400

    sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -15,22 +15,22 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
     console.log('blogs slugs',blogsSlugs)
 
 // //cars slugs
-    const carsRes = await fetcher<{success:boolean,cars:{id:string,slug:string,company:{slug:string}}[]}>(GET_CARS_SEO)
-    const carsSlugs:MetadataRoute.Sitemap = carsRes.cars?.map(car=>({url:`${process.env.NEXT_PUBLIC_BASE_URL}/${car.company.slug}/${car.slug}`}))
-    console.log('cars slugs',carsSlugs)
+    // const carsRes = await fetcher<{success:boolean,cars:{id:string,slug:string,company:{slug:string}}[]}>(GET_CARS_SEO)
+    // const carsSlugs:MetadataRoute.Sitemap = carsRes.cars?.map(car=>({url:`${process.env.NEXT_PUBLIC_BASE_URL}/${car.company.slug}/${car.slug}`}))
+    // console.log('cars slugs',carsSlugs)
 
 // //companies slugs
-    // const companiesRes = await fetcher<{success:boolean,companies:{id:string,slug:string}[]}>(GET_COMPANIES)
-    // const companiesSlugs:MetadataRoute.Sitemap = companiesRes.companies?.map(company=>({url:`${process.env.NEXT_PUBLIC_BASE_URL}/${company.slug}`}))
-    // console.log('companies slugs',companiesSlugs)
+    const companiesRes = await fetcher<{success:boolean,companies:{id:string,slug:string}[]}>(GET_COMPANIES)
+    const companiesSlugs:MetadataRoute.Sitemap = companiesRes.companies?.map(company=>({url:`${process.env.NEXT_PUBLIC_BASE_URL}/${company.slug}`}))
+    console.log('companies slugs',companiesSlugs)
 
 
 
 
   return [
     ...blogsSlugs,
-    ...carsSlugs,
-    // ...companiesSlugs,
+    // ...carsSlugs,
+    ...companiesSlugs,
     { url: process.env.NEXT_PUBLIC_BASE_URL as string },
     { url: `${process.env.NEXT_PUBLIC_BASE_URL}/blog` },
     { url: `${process.env.NEXT_PUBLIC_BASE_URL}/about-us` },
