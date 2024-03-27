a4e55f2535ed435880fd2315e213244bc71865df
ammar <ammar@wdodigital.com>
2024-03-27 10:15:35 +0400

    refine

--- components/(main info)/main-info.tsx
+++ components/(main info)/main-info.tsx
@@ -25,7 +25,7 @@ const MainInfo = (props: Props) => {
   return (
     <section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-20">
       {info.map((el) => (
-        <article className="flex flex-col  sm:items-start ">
+        <article key={el.title} className="flex flex-col  sm:items-start ">
           <div className="flex items-center gap-4 ">
             <div className="relative w-12 h-12">
               <Image src={el.icon} fill alt="icon" className="object-contain" />
