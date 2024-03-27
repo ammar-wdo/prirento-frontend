bf08b2dc76ee120488e9a49f98b54c32c0efc51f
ammar <ammar@wdodigital.com>
2024-03-27 13:26:33 +0400

    refine

--- components/(banner)/banner.tsx
+++ components/(banner)/banner.tsx
@@ -7,10 +7,10 @@ type Props = {};
 
 const Banner = (props: Props) => {
   return (
-    <div className="bg-main md:pb-32 pb-56 h-[500px] relative">
-      <Image src={'/banner-image.jpg'} fill alt='banner-image' className="object-cover" />
+    <div className="bg-main md:pb-32 pb-56 h-[450px] relative">
+      <Image src={'/banner-image.jpg'} fill alt='banner-image' className="object-cover object-bottom" />
          <MainHeader transparent={true} />
-         <div className="container">  <h1 className="capitalize text-white  text-left font-semibold pt-2 md:pt-12 text-2xl md:text-3xl lg:text-[40px] relative xl:mt-44 mt-20">
+         <div className="container">  <h1 className="capitalize text-white  text-left font-semibold pt-2 md:pt-12 text-2xl md:text-3xl lg:text-[40px] relative  xl:mt-32 mt-8">
       Find & compare luxury cars <br/>In one click.
      
       </h1></div>
