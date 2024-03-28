1ad0ea415806e0b50719383f81d4b495a42457a4
ammar <ammar@wdodigital.com>
2024-03-28 09:37:45 +0400

    refine

--- components/(banner)/banner.tsx
+++ components/(banner)/banner.tsx
@@ -7,14 +7,16 @@ type Props = {};
 
 const Banner = (props: Props) => {
   return (
-    <div className="bg-main md:pb-32 pb-56 h-[450px] relative">
+    <div className="bg-main md:pb-32 pb-56 h-[550px] relative">
       <Image priority={true} src={'/banner-image.jpg'} fill alt='banner-image' className="object-cover object-bottom hidden xl:block" />
       <Image priority={true} src={'/banner-mobile.jpg'} fill alt='banner-image' className="object-cover  xl:hidden object-center" />
          <MainHeader transparent={true} />
-         <div className="container">  <h1 className="capitalize text-white  text-left font-semibold pt-2 md:pt-12 text-2xl md:text-3xl lg:text-[40px] relative  xl:mt-32 mt-8">
-      Find & compare luxury cars <br/>In one click.
+         <div className="container">  <h1 className="capitalize text-white  text-left font-semibold text-2xl md:text-3xl lg:text-[40px] w-fit relative  xl:mt-32 mt-8 sm:bg-main">
+      Find & compare luxury cars
      
-      </h1></div>
+      </h1>
+      <h2 className="capitalize text-white  text-left font-semibold text-2xl md:text-3xl lg:text-[40px] w-fit relative  mt-2 sm:bg-main">In one click.</h2>
+      </div>
     
    
     </div>
--- components/(main info)/main-info.tsx
+++ components/(main info)/main-info.tsx
@@ -25,14 +25,14 @@ const MainInfo = (props: Props) => {
   return (
     <section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-20">
       {info.map((el) => (
-        <article key={el.title} className="flex flex-col  sm:items-start ">
+        <article key={el.title} className="flex flex-col   ">
           <div className="flex items-center gap-4 ">
             <div className="relative w-12 h-12">
               <Image src={el.icon} fill alt="icon" className="object-contain" />
             </div>
             <h3 className="capitalize font-bold text-[17px]">{el.title}</h3>
           </div>
-          <p className="mt-6 text-[25px] font-bold">{el.description}</p>
+          <p className="mt-6 sm:text-[25px] text-base font-bold ">{el.description}</p>
         </article>
       ))}
     </section>
